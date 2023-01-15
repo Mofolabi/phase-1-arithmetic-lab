7498d7b2bae63d7ed81cfae79df3865fae5f8334
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-15 23:13:39 +0100

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+let multiply = num1*num2
+let random = Math.floor(Math.random() * 1000);
+let num3 = 22
+let num4 = 9
+let mod = num3%num4
+let max = (1,20)
